8e50d80f5769ae42df32923c0c5444c05b30e39e
Indranil Karmakar <55432757+indranil316@users.noreply.github.com>
2023-06-17 12:01:33 +0530

    fixed mobile nav

--- src/components/Header/MobileNav.js
+++ src/components/Header/MobileNav.js
@@ -49,8 +49,9 @@ function MobileNav(props) {
                               <Disclosure.Button
                                 key={item.id}
                                 as={Link}
-                                to={'shop'+item.slug}
+                                to={'/shop/'+item.slug}
                                 className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                onClick={() => setMobileMenuOpen(false)}
                               >
                                 {item.name}
                               </Disclosure.Button>
